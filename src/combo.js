455782ee9e70d274dda0b534d134acb001e4fc0b
Claire Song <claireyuan.song@gmail.com>
2024-03-16 19:59:35 +0100

    cards are not generated

--- src/combo.js
+++ src/combo.js
@@ -1,3 +1,5 @@
+import cardStack from "./cardstack.js";
+
 class GenerateCombo {
   constructor(cardContainer) {
     // takes argument of the card container div in the index.html file
@@ -13,13 +15,12 @@ class GenerateCombo {
       this.cardContainer.append(newCard);
       newCard.classList.add("card");
       // newCard.classList.add(`cardNb${i}`);
-      const cardNumber = document.createElement("div");
-      newCard.append(cardNumber);
-      this.cardValue(cardNumber);
+      this.style(newCard);
       this.cardsArray.push(newCard);
     }
   }
 
+  // removes the card div elements when the combo is over
   comboOver() {
     this.cardsArray.forEach((card) => {
       card.remove();
@@ -27,9 +28,18 @@ class GenerateCombo {
     this.cardsArray = [];
   }
 
+  // styles the card according to the value
+  style(card) {
+    const value = this.generateNumber();
+    // fetch the image according to the number
+    card.style.backgroundImage = `url(${cardStack.clubs[value - 1].image})`;
+  }
+
+  // determines the value for the card between 1 and 10
   cardValue(card) {
     const value = this.generateNumber();
     card.textContent = value;
+    // card.style.backgroundImage = `url(${cardStack.clubs[0].image})`;
   }
 
   generateNumber() {
