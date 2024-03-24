86c807c73ea89eecc83b762d0150ccd80b39644b
Claire Song <claireyuan.song@gmail.com>
2024-03-24 16:52:15 +0100

    trying to modify html directly

---
+++ react-24/src/assets/stack.tsx
@@ -0,0 +1,207 @@
+const cardStackData = [
+  {
+    value: 1,
+    image: "https://claire2d2.github.io/game-find-24/img/ace_of_hearts.png",
+    family: "hearts",
+  },
+  {
+    value: 2,
+    image: "https://claire2d2.github.io/game-find-24/img/2_of_hearts.png",
+    family: "hearts",
+  },
+  {
+    value: 3,
+    image: "https://claire2d2.github.io/game-find-24/img/3_of_hearts.png",
+    family: "hearts",
+  },
+  {
+    value: 4,
+    image: "https://claire2d2.github.io/game-find-24/img/4_of_hearts.png",
+    family: "hearts",
+  },
+  {
+    value: 5,
+    image: "https://claire2d2.github.io/game-find-24/img/5_of_hearts.png",
+    family: "hearts",
+  },
+  {
+    value: 6,
+    image: "https://claire2d2.github.io/game-find-24/img/6_of_hearts.png",
+    family: "hearts",
+  },
+  {
+    value: 7,
+    image: "https://claire2d2.github.io/game-find-24/img/7_of_hearts.png",
+    family: "hearts",
+  },
+  {
+    value: 8,
+    image: "https://claire2d2.github.io/game-find-24/img/8_of_hearts.png",
+    family: "hearts",
+  },
+  {
+    value: 9,
+    image: "https://claire2d2.github.io/game-find-24/img/9_of_hearts.png",
+    family: "hearts",
+  },
+  {
+    value: 10,
+    image: "https://claire2d2.github.io/game-find-24/img/10_of_hearts.png",
+    family: "hearts",
+  },
+  // SPADES
+  {
+    value: 1,
+    image: "https://claire2d2.github.io/game-find-24/img/ace_of_spades.png",
+    family: "spades",
+  },
+  {
+    value: 2,
+    image: "https://claire2d2.github.io/game-find-24/img/2_of_spades.png",
+    family: "spades",
+  },
+  {
+    value: 3,
+    image: "https://claire2d2.github.io/game-find-24/img/3_of_spades.png",
+    family: "spades",
+  },
+  {
+    value: 4,
+    image: "https://claire2d2.github.io/game-find-24/img/4_of_spades.png",
+    family: "spades",
+  },
+  {
+    value: 5,
+    image: "https://claire2d2.github.io/game-find-24/img/5_of_spades.png",
+    family: "spades",
+  },
+  {
+    value: 6,
+    image: "https://claire2d2.github.io/game-find-24/img/6_of_spades.png",
+    family: "spades",
+  },
+  {
+    value: 7,
+    image: "https://claire2d2.github.io/game-find-24/img/7_of_spades.png",
+    family: "spades",
+  },
+  {
+    value: 8,
+    image: "https://claire2d2.github.io/game-find-24/img/8_of_spades.png",
+    family: "spades",
+  },
+  {
+    value: 9,
+    image: "https://claire2d2.github.io/game-find-24/img/9_of_spades.png",
+    family: "spades",
+  },
+  {
+    value: 10,
+    image: "https://claire2d2.github.io/game-find-24/img/10_of_spades.png",
+    family: "spades",
+  },
+  // DIAMONDS
+  {
+    value: 1,
+    image: "https://claire2d2.github.io/game-find-24/img/ace_of_diamonds.png",
+    family: "diamonds",
+  },
+  {
+    value: 2,
+    image: "https://claire2d2.github.io/game-find-24/img/2_of_diamonds.png",
+    family: "diamonds",
+  },
+  {
+    value: 3,
+    image: "https://claire2d2.github.io/game-find-24/img/3_of_diamonds.png",
+    family: "diamonds",
+  },
+  {
+    value: 4,
+    image: "https://claire2d2.github.io/game-find-24/img/4_of_diamonds.png",
+    family: "diamonds",
+  },
+  {
+    value: 5,
+    image: "https://claire2d2.github.io/game-find-24/img/5_of_diamonds.png",
+    family: "diamonds",
+  },
+  {
+    value: 6,
+    image: "https://claire2d2.github.io/game-find-24/img/6_of_diamonds.png",
+    family: "diamonds",
+  },
+  {
+    value: 7,
+    image: "https://claire2d2.github.io/game-find-24/img/7_of_diamonds.png",
+    family: "diamonds",
+  },
+  {
+    value: 8,
+    image: "https://claire2d2.github.io/game-find-24/img/8_of_diamonds.png",
+    family: "diamonds",
+  },
+  {
+    value: 9,
+    image: "https://claire2d2.github.io/game-find-24/img/9_of_diamonds.png",
+    family: "diamonds",
+  },
+  {
+    value: 10,
+    image: "https://claire2d2.github.io/game-find-24/img/10_of_diamonds.png",
+    family: "diamonds",
+  },
+  // CLUBS
+  {
+    value: 1,
+    image: "https://claire2d2.github.io/game-find-24/img/ace_of_clubs.png",
+    family: "clubs",
+  },
+  {
+    value: 2,
+    image: "https://claire2d2.github.io/game-find-24/img/2_of_clubs.png",
+    family: "clubs",
+  },
+  {
+    value: 3,
+    image: "https://claire2d2.github.io/game-find-24/img/3_of_clubs.png",
+    family: "clubs",
+  },
+  {
+    value: 4,
+    image: "https://claire2d2.github.io/game-find-24/img/4_of_clubs.png",
+    family: "clubs",
+  },
+  {
+    value: 5,
+    image: "https://claire2d2.github.io/game-find-24/img/5_of_clubs.png",
+    family: "clubs",
+  },
+  {
+    value: 6,
+    image: "https://claire2d2.github.io/game-find-24/img/6_of_clubs.png",
+    family: "clubs",
+  },
+  {
+    value: 7,
+    image: "https://claire2d2.github.io/game-find-24/img/7_of_clubs.png",
+    family: "clubs",
+  },
+  {
+    value: 8,
+    image: "https://claire2d2.github.io/game-find-24/img/8_of_clubs.png",
+    family: "clubs",
+  },
+  {
+    value: 9,
+    image: "https://claire2d2.github.io/game-find-24/img/9_of_clubs.png",
+    family: "clubs",
+  },
+  {
+    value: 10,
+    image: "https://claire2d2.github.io/game-find-24/img/10_of_clubs.png",
+    family: "clubs",
+  },
+];
+
+export default cardStackData;
